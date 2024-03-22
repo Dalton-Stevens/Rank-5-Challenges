beb06510d62c2e0b52dabfa013f5e69a5ed79740
Dalton Stevens <d.72.stevens@gmail.com>
2024-03-21 19:46:15 -0500

    code wars challenge

---
+++ longest.js
@@ -0,0 +1,7 @@
+const longest = (s1, s2) => {
+  return [...new Set(s1 + s2)].sort().join("");
+};
+
+console.log(longest("aretheyhere", "yestheyarehere"));
+console.log(longest("loopingisfunbutdangerous", "lessdangerousthancoding"));
+console.log(longest("inmanylanguages", "theresapairoffunctions"));
