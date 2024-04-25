5abfc77e61c1ad0896de500ca6eb99dbc193d5d8
Dalton Stevens <d.72.stevens@gmail.com>
2024-04-24 21:48:20 -0500

    code wars challenge

---
+++ findEvenIndex.js
@@ -0,0 +1,15 @@
+const findEvenIndex = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    const leftSum = arr.slice(0, i).reduce((acc, curr) => acc + curr, 0);
+    const rightSum = arr.slice(i + 1).reduce((acc, curr) => acc + curr, 0);
+
+    if (leftSum === rightSum) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+console.log(findEvenIndex([1, 2, 3, 4, 3, 2, 1]));
+console.log(findEvenIndex([1, 100, 50, -51, 1, 1]));
+console.log(findEvenIndex([1, 2, 3, 4, 5, 6]));
